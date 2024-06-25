b21033456cac0434fa2725b0bd6eef116777dede
cj0311 <quritis@naver.com>
2024-06-25 23:22:19 +0900

    Feat aws 가기 직전 커밋

--- src/main/java/org/spring/webproject/web/IndexController.java
+++ src/main/java/org/spring/webproject/web/IndexController.java
@@ -1,6 +1,8 @@
 package org.spring.webproject.web;
 
 import lombok.RequiredArgsConstructor;
+import org.spring.webproject.config.auth.LoginUser;
+import org.spring.webproject.config.auth.dto.SessionUser;
 import org.spring.webproject.service.PostService;
 import org.spring.webproject.web.dto.PostsResponseDto;
 import org.springframework.stereotype.Controller;
@@ -8,15 +10,25 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @Controller
 @RequiredArgsConstructor
 public class IndexController {
 
     private final PostService postService;
+    private final HttpSession httpSession;
+
 
     @GetMapping("/")
-    public String index(Model model) {
+
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postService.findAllDesc());
+
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
+
         return "index";
     }
 
--- src/test/java/org/spring/webproject/web/HelloControllerTest.java
+++ src/test/java/org/spring/webproject/web/HelloControllerTest.java
@@ -2,8 +2,12 @@ package org.spring.webproject.web;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.spring.webproject.config.auth.SecurityConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -12,7 +16,11 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.hamcrest.Matchers.is;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(controllers = HelloController.class)
+@WebMvcTest(controllers = HelloController.class,
+excludeFilters = {
+        @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE
+        ,classes = SecurityConfig.class)
+})
 public class HelloControllerTest {
 
 
@@ -20,6 +28,7 @@ public class HelloControllerTest {
     private MockMvc mvc;
 
     @Test
+    @WithMockUser(roles = "USER")
     public void hello가_리턴된다() throws Exception{
         String hello = "hello";
         mvc.perform(get("/hello"))
@@ -29,6 +38,7 @@ public class HelloControllerTest {
 
 
     @Test
+    @WithMockUser(roles = "USER")
     public void helloDto가_리턴된다() throws Exception{
         String name = "hello";
         int amount = 1000;
